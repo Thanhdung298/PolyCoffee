936742b393e53938cb0af35dfcc5dae2b2e5eab7
hailainhai123 <44771709+hailainhai123@users.noreply.github.com>
2024-01-13 15:16:06 +0700

    update

--- app/src/main/java/com/example/polycoffee/activity/LoginActivity.kt
+++ app/src/main/java/com/example/polycoffee/activity/LoginActivity.kt
@@ -10,8 +10,8 @@ import com.example.polycoffee.dao.TempFunc
 import com.example.polycoffee.databinding.ActivityLoginBinding
 import com.example.polycoffee.model.User
 
-class LoginActivity : AppCompatActivity(){
-    lateinit var binding:ActivityLoginBinding
+class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
     private lateinit var dao: DAO
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,34 +20,36 @@ class LoginActivity : AppCompatActivity(){
         supportActionBar?.hide()
         dao = DAO(this)
         val preferences = getSharedPreferences("USER ACCOUNT", MODE_PRIVATE)
-        binding.edLoginUsername.editText!!.setText(preferences.getString("username",""))
-        binding.edLoginPassword.editText!!.setText(preferences.getString("password",""))
-        binding.rdoLoginRMB.isChecked = preferences.getBoolean("check",false)
+        binding.edLoginUsername.editText!!.setText(preferences.getString("username", ""))
+        binding.edLoginPassword.editText!!.setText(preferences.getString("password", ""))
+        binding.rdoLoginRMB.isChecked = preferences.getBoolean("check", false)
         binding.btnLogin.setOnClickListener {
             checkLogin()
         }
 
 
     }
-    fun rememberUser(user: String,password:String,check:Boolean){
+
+    fun rememberUser(user: String, password: String, check: Boolean) {
         val preferences = getSharedPreferences("USER ACCOUNT", MODE_PRIVATE)
         val edit = preferences.edit()
-        if (!check){
+        if (!check) {
             edit.clear()
-        }else{
-            edit.putString("username",user)
-            edit.putString("password",password)
-            edit.putBoolean("check",check)
+        } else {
+            edit.putString("username", user)
+            edit.putString("password", password)
+            edit.putBoolean("check", check)
         }
         edit.apply()
     }
-    private fun checkLogin(){
+
+    private fun checkLogin() {
         val username = binding.edLoginUsername.editText!!.text.toString()
         val password = binding.edLoginPassword.editText!!.text.toString()
         val checkbox = binding.rdoLoginRMB
-        TempFunc.checkField(binding.edLoginUsername,binding.edLoginPassword)
-        if(TempFunc.noError(binding.edLoginUsername,binding.edLoginPassword)){
-            val database= FirebaseDatabaseTemp.getDatabase()!!.getReference("User")
+        TempFunc.checkField(binding.edLoginUsername, binding.edLoginPassword)
+        if (TempFunc.noError(binding.edLoginUsername, binding.edLoginPassword)) {
+            val database = FirebaseDatabaseTemp.getDatabase()!!.getReference("User")
             val intent = Intent(this, MainActivity::class.java)
             database.get().addOnSuccessListener {
                 if (it.child(username).value != null){
@@ -69,8 +71,22 @@ class LoginActivity : AppCompatActivity(){
                     Toast.makeText(this@LoginActivity,"Sai tên đăng nhập",Toast.LENGTH_SHORT).show()
                 }
             } .addOnFailureListener {
-                Toast.makeText(this,"Không có kết nối mạng",Toast.LENGTH_SHORT).show()
+               /* intent.putExtra("role",1)
+                startActivity(intent)
+                finish()
+                database.onDisconnect()*/
+               Toast.makeText(this,"Không có kết nối mạng",Toast.LENGTH_SHORT).show()
             }
+          /*  database.get().addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this@LoginActivity, "lấy dữ liệu oke", Toast.LENGTH_LONG).show()
+                } else {
+                    var ex = task.exception;
+                    Toast.makeText(this@LoginActivity, "lỗi: ${ex?.message}", Toast.LENGTH_LONG)
+                        .show()
+                }
+            }*/
+
             database.keepSynced(false)
         }
     }
